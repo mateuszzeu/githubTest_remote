be75a29b4d9ca8cbdf3c1324563240c0d3bda405
xzeu <mateuszzeu@gmail.com>
2023-04-03 13:21:47 +0200

    Initial Commit

---
+++ githubTest/githubTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  githubTestApp.swift
+//  githubTest
+//
+//  Created by Mateusz Żełudziewicz on 03/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct githubTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
