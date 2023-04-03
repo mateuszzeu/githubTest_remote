d58ec0dc61745f9f79b75bc63fb3194ae0d8bb34
xzeu <mateuszzeu@gmail.com>
2023-04-03 13:28:38 +0200

    Create second view

---
+++ githubTest/SecondView.swift
@@ -0,0 +1,20 @@
+//
+//  SecondView.swift
+//  githubTest
+//
+//  Created by Mateusz Żełudziewicz on 03/04/2023.
+//
+
+import SwiftUI
+
+struct SecondView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SecondView_Previews: PreviewProvider {
+    static var previews: some View {
+        SecondView()
+    }
+}
